a7aedf247dee0592efb487942a6d1bc7148ad299
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-11-13 11:54:09 -0500

    BubblePage is requesting data from the api using axiosWithAuth(). data is being stored to state and displayed properly.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+
+import axiosWithAuth from "./../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const getColorListData = () => {
+    axiosWithAuth().get('/colors')
+      .then((req => {
+        setColorList(req.data);
+      }))
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+
+  useEffect(() => {
+    getColorListData();
+  }, [])
 
   return (
     <>
